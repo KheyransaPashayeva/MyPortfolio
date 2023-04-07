6468bd5f21780de55f731956f72f2d34f64bc0d7
Kheyransa Pashayeva <xeyransa.pshayeva@gmail.com>
2023-04-07 21:40:01 +0400

    final

---
+++ static/js/open_menu.js
@@ -0,0 +1,10 @@
+
+function open_menu(){
+    document.getElementById("openmenu").classList.add("active");
+}
+
+function close_menu(){
+    document.getElementById("openmenu").classList.remove("active"); 
+}
+
+
